8a4e2e753556768ac7b04142b5d802b99d87aa2d
pvanliefland <pierre.vanliefland@gmail.com>
2025-12-01 19:34:15 +0100

    chore: cleanup

--- src/aoc2025/day01.py
+++ src/aoc2025/day01.py
@@ -11,21 +11,21 @@ def puzzle() -> tuple[tuple[int, int], tuple[int, int]]:
 def compute_password(file_path: str) -> tuple[int, int]:
     """Compute password (part 1 and part 2)."""
     with Path(f"inputs/{file_path}").open() as example_input_file:
-        example_input = example_input_file.read()
-    position, password_1, password_2 = 50, 0, 0
+        data = example_input_file.read()
+    position, password1, password2 = 50, 0, 0
 
-    for line in example_input.splitlines():
+    for line in data.splitlines():
         if line[0] == "L":
-            dist_to_zero, rotation = position, -int(line[1:])
+            distance, rotation = position, -int(line[1:])
         else:
-            dist_to_zero, rotation = (100 - position), int(line[1:])
+            distance, rotation = (100 - position), int(line[1:])
 
-        if abs(rotation) % 100 == dist_to_zero:
-            password_1 += 1
-        if abs(rotation) >= dist_to_zero:
-            password_2 += (0 if dist_to_zero == 0 else 1) + int(
-                (abs(rotation) - dist_to_zero) / 100
+        if abs(rotation) % 100 == distance:
+            password1 += 1
+        if abs(rotation) >= distance:
+            password2 += (0 if distance == 0 else 1) + int(
+                (abs(rotation) - distance) / 100
             )
         position = (position + rotation) % 100
 
-    return password_1, password_2
+    return password1, password2
